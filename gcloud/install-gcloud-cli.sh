6c159d20b9de3621a69df79e300f0d2688c0c804
leandroid <leandrosiow@users.noreply.github.com>
2024-04-23 14:37:57 -0700

    Updated install-gcloud-cli.sh

--- gcloud/install-gcloud-cli.sh
+++ gcloud/install-gcloud-cli.sh
@@ -1,1 +1,1 @@
-
+sudo echo "deb [signed-by=/usr/share/keyrings/cloud.google.asc] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.asc && sudo apt-get update -y && sudo apt-get install google-cloud-sdk -y
